e0c3aba1f9f0798e2ca5f554dd201c26fb7a0a82
Roman <broilerster@gmail.com>
2012-05-15 21:37:29 +0300

    Merged all the migrations into one. Moved migration code to generator template

--- lib/generators/mailboxer/install_generator.rb
+++ lib/generators/mailboxer/install_generator.rb
@@ -17,19 +17,6 @@ class Mailboxer::InstallGenerator < Rails::Generators::Base #:nodoc:
   end
 
   def copy_migrations
-    if Rails.version < "3.1"
-      migrations = [["20110511145103_create_mailboxer.rb","create_mailboxer.rb"],
-                    ["20110719110700_add_notified_object.rb","add_notified_object.rb"],
-                    ["20110912163911_add_notification_code.rb","add_notification_code.rb"],
-                    ["20111204163911_add_attachments.rb","add_attachments.rb"]]
-      migrations.each do |migration|
-        migration_template "../../../../db/migrate/" + migration[0], "db/migrate/" + migration[1]
-      end
-    else
-      require 'rake'
-      Rails.application.load_tasks
-      Rake::Task['railties:install:migrations'].reenable
-      Rake::Task['mailboxer_engine:install:migrations'].invoke
-    end
+    migration_template "migration.rb", "db/migrate/create_mailboxer.rb"
   end
 end
--- spec/dummy/db/migrate/20120305103203_add_attachments.rb
+++
@@ -1,5 +0,0 @@
-class AddAttachments < ActiveRecord::Migration
-  def change
-    add_column :notifications, :attachment, :string
-  end
-end
