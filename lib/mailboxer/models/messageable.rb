38ebdb0ef27ab05a4cd35929339b8d69bd232d91
Matthew Carr <bikokid@gmail.com>
2012-09-19 15:36:15 -0700

    changing method name to resolve conflict with active record

--- lib/mailboxer/models/messageable.rb
+++ lib/mailboxer/models/messageable.rb
@@ -122,7 +122,7 @@ module Mailboxer
       #* A Notification
       #* A Conversation
       #* An array with any of them
-      def read(obj)
+      def mark_as_read(obj)
         case obj
         when Receipt
           return obj.mark_as_read if obj.receiver == self
