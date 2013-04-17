adf345b603dec31dcf302cee0115cd7509d47b66
Mike Snare <mikesnare@gmail.com>
2013-04-17 07:59:44 -0400

    Remove support for delete notifications.

--- db/migrate/20130322162134_add_notification_deleted_support.rb
+++
@@ -1,9 +0,0 @@
-class AddNotificationDeletedSupport < ActiveRecord::Migration
-
-  def change
-    change_table :notifications do |t|
-      t.boolean :deleted, :default => false
-    end
-  end
-
-end
