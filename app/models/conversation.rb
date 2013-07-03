5d26b848d2d624af94fb07d9db1d03f460fd8faa
Justin Coyne <justin@curationexperts.com>
2013-07-03 16:34:54 -0500

    Only call attr_accessible if it's rails 3

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,5 @@
 class Conversation < ActiveRecord::Base
-  attr_accessible :subject
+  attr_accessible :subject if Rails::VERSION::MAJOR == 3
 
   has_many :messages, :dependent => :destroy
   has_many :receipts, :through => :messages
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < Notification
-  attr_accessible :attachment
+  attr_accessible :attachment if Rails::VERSION::MAJOR == 3
 
   belongs_to :conversation, :validate => true, :autosave => true
   validates_presence_of :sender
