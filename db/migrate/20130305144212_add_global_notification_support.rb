7d02e097bd34f6931283c3fe56588396dfab518e
David Rodríguez de Dios <deivid.rodriguez@gmail.com>
2013-10-16 19:05:42 +0200

    Squash all mailboxer migrations into a single one
    Mailboxer user doesn't need to know about internal details of mailboxer
    development history. This task was half implemented in commit
    1901023668d087b0bd4ca498e63e5c1c0779e001, but for some reason only some
    migrations were merged into the first one. Merging this would also close
    PR #140.

--- db/migrate/20130305144212_add_global_notification_support.rb
+++
@@ -1,9 +0,0 @@
-class AddGlobalNotificationSupport < ActiveRecord::Migration
-
-  def change
-    change_table :mailboxer_notifications do |t|
-      t.boolean :global, default: false
-      t.datetime :expires
-    end
-  end
-end
--- spec/dummy/db/migrate/20120813110712_rename_receipts_read.rb
+++
@@ -1,9 +0,0 @@
-class RenameReceiptsRead < ActiveRecord::Migration
-  def up
-    rename_column :mailboxer_receipts, :read, :is_read
-  end
-
-  def down
-    rename_column :mailboxer_receipts, :is_read, :read
-  end
-end
