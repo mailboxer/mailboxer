dc837ae633379fc865d5b1bf3e95bc693ba862c4
David Rodríguez <deivid.rodriguez@gmail.com>
2013-12-09 10:07:36 +0100

    Grammar changes
    Not an expert in English grammar but this should be better. Probably a native speaker can further improve this... :S

--- lib/generators/mailboxer/templates/initializer.rb
+++ lib/generators/mailboxer/templates/initializer.rb
@@ -1,16 +1,16 @@
 Mailboxer.setup do |config|
 
-  #Configures if you applications uses or no the email sending for Notifications and Messages
+  #Configures if you application uses or not email sending for Notifications and Messages
   config.uses_emails = true
 
-  #Configures the default from for the email sent for Messages and Notifications of Mailboxer
+  #Configures the default from for emails sent for Messages and Notifications
   config.default_from = "no-reply@mailboxer.com"
 
   #Configures the methods needed by mailboxer
   config.email_method = :mailboxer_email
   config.name_method = :name
 
-  #Configures if you use or not a search engine and which one are you using
+  #Configures if you use or not a search engine and which one you are using
   #Supported engines: [:solr,:sphinx]
   config.search_enabled = false
   config.search_engine = :solr
