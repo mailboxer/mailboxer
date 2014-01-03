a54229d9d339727a44f4da1e62c7801c044e344e
John DeWyze <jdewzy@gmail.com>
2014-01-03 00:30:08 -0600

    Namespace index fix.

--- lib/generators/mailboxer/templates/mailboxer_namespacing_compatibility.rb
+++ lib/generators/mailboxer/templates/mailboxer_namespacing_compatibility.rb
@@ -6,8 +6,8 @@ class MailboxerNamespacingCompatibility < ActiveRecord::Migration
     rename_table :receipts,      :mailboxer_receipts
 
     if Rails.version < '4'
-      rename_index :mailboxer_notifications, :notifications_on_conversation_id, :mailboxer_notifications_on_conversation_id
-      rename_index :mailboxer_receipts,      :receipts_on_notification_id,      :mailboxer_receipts_on_notification_id
+      rename_index :mailboxer_notifications, "index_notifications_on_conversation_id", :mailboxer_notifications_on_conversation_id
+      rename_index :mailboxer_receipts,      "index_receipts_on_notification_id",      :mailboxer_receipts_on_notification_id
     end
   end
 
