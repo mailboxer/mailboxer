73f533be5cac795294e373506de42bc685b4e198
seankay <f.sean.kay@gmail.com>
2014-12-04 17:49:59 -0500

    Upgrade to rspec 3. Add rspec-its and rspec-collection_matchers gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rspec/rails"
 
+require 'rspec/its'
+require 'rspec/collection_matchers'
+
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
