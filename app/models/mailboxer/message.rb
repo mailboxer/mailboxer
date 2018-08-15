972fa36e1a58d0f5a104f82cfc2ee9ea2fa0bc1f
Nauman Zahid <nauman.goldsmith@gmail.com>
2018-08-15 17:20:18 +0500

    Completed multiple attachments feature and fix migration issue for Rails 5.2
    Signed-off-by: Nauman Zahid <nauman.goldsmith@gmail.com>

--- app/models/mailboxer/message.rb
+++ app/models/mailboxer/message.rb
@@ -1,5 +1,4 @@
 class Mailboxer::Message < Mailboxer::Notification
-  attr_accessible :attachment if Mailboxer.protected_attributes? and !Mailboxer.uses_multiple_attachments
   self.table_name = :mailboxer_notifications
 
   belongs_to :conversation, :validate => true, :autosave => true
@@ -11,9 +10,7 @@ class Mailboxer::Message < Mailboxer::Notification
     where(:conversation_id => conversation.id)
   }
 
-  has_many :attachments, class_name: 'Mailboxer::Attachment', foreign_key: :notification_id,
-           dependent: :destroy if Mailboxer.uses_multiple_attachments
-  mount_uploader :attachment, Mailboxer::AttachmentUploader unless Mailboxer.uses_multiple_attachments
+  include Mailboxer::Attachable
 
   class << self
     #Sets the on deliver callback method.
@@ -47,17 +44,5 @@ class Mailboxer::Message < Mailboxer::Notification
       on_deliver_callback.call(self) if on_deliver_callback
     end
     sender_receipt
-  end
-
-  if Mailboxer.uses_multiple_attachments
-    #This method is defined so that Message builder works with
-    #multiple attachments. It uses attachment= method.
-    def attachment=(attached_files)
-      attached_files = [*attached_files]
-      self.attachments.destroy # Replacing old attachments with new ones
-      attached_files.each do |file|
-        self.attachments << Mailboxer::Attachment.new(file: file)
-      end
-    end
   end
 end
