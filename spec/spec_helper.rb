7fe031336774639fe2857973d676a6feed94828a
Uysim Ty <uysimty@gmail.com>
2018-09-06 15:33:54 +0700

    add doc and spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ Capybara.default_driver   = :rack_test
 Capybara.default_selector = :css
 
 # Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
+ActiveRecord::MigrationContext.new(File.expand_path('../dummy/db/migrate', __FILE__)).migrate
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
