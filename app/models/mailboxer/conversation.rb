fc9af0fcbdc5264d42f485e43afa02eab6575f45
Mark Chavez <mjfchavez@gmail.com>
2023-09-05 13:32:39 +0800

    fix: ensure participant is present

--- app/models/mailboxer/conversation.rb
+++ app/models/mailboxer/conversation.rb
@@ -207,6 +207,12 @@ class Mailboxer::Conversation < ActiveRecord::Base
 
   # tells if participant is opt in
   def has_subscriber?(participant)
+    return false if participant.nil?
+    !opt_outs.unsubscriber(participant).any?
+  end
+
+  # tells if participant is opt in
+  def has_subscriber?(participant)
     !opt_outs.unsubscriber(participant).any?
   end
 
